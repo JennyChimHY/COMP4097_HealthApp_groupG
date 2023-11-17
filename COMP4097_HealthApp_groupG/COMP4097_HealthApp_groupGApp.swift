9a6a07a3f1be36d2acb66b8c1c736bdb8c9172ea
JennyChimHY <chimhoiyan@gmail.com>
2023-11-17 15:10:55 +0800

    Initial Commit

---
+++ COMP4097_HealthApp_groupG/COMP4097_HealthApp_groupGApp.swift
@@ -0,0 +1,17 @@
+//
+//  COMP4097_HealthApp_groupGApp.swift
+//  COMP4097_HealthApp_groupG
+//
+//  Created by f0226942 on 17/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct COMP4097_HealthApp_groupGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ COMP4097_HealthApp_groupG/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  COMP4097_HealthApp_groupG
+//
+//  Created by f0226942 on 17/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
